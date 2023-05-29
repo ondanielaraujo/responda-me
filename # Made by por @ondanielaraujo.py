2adb712df21d0aa2a12259ac44858eb5de824e52
Daniel Araújo <132944794+ondanielaraujo@users.noreply.github.com>
2023-05-29 14:46:01 -0300

    Add files via upload

---
+++ # Made by por @ondanielaraujo.py
@@ -0,0 +1,16 @@
+# Made by por @ondanielaraujo
+import math
+
+years = int(input("Que ano você quer casar?[2023-2150]"))
+input ("Você vai ser fiel ao seu companheiro?")
+input ("E ter filhos, você pretende?")
+input ("Chute um número dentro de [ 2023-2150]")
+numero = [2023, 2150,3000]
+for valor in numero:
+    if valor > 3000:
+        print("Você já estará morto")
+        break
+    else:
+        print("O tempo está passando")
+        break
+    
